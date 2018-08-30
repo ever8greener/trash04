d4ec4d9c34792d821850b381831aa5a7fce87a06
eddiekwon <eddiekwon7@gmail.com>
2018-08-30 17:25:09 +0900

    Initial Commit

---
+++ trash04/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  trash04
+//
+//  Created by eddiek on 30/08/2018.
+//  Copyright © 2018 Eddie Kwon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
